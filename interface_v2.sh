0cf1871d64da108d403f99ee25e2e4207d574c28
En3ide <38477527+En3ide@users.noreply.github.com>
2024-10-03 15:14:12 +0200

    kn

--- interface_v2.sh
+++ interface_v2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 clear
 #pour les couleur tu peux aussi utiliser "\033[38;5<code couleurs>m" code en 256
 
@@ -11,29 +11,29 @@ reset="\033[0m"
 
 carre_plein="\u2588"
 carre_vide="\u25A1"
-#if (( cols < 50 )); then 
-#if (( lines < 30 ))
+#if [ "$cols" -lt 50 ]; then 
+#if [ "$lines" -lt 30 ]
 
 clear_screen() {
 	cols=$(tput cols)
 	lines=$(tput lines)
-	for ((i=1;i<=$cols;i++)); do
-		for ((j=1;j<=$lines;j++)); do
-			if (( i == 1 )); then
+	for i in $(seq 1 "$cols"); do
+		for j in $(seq 1 "$lines"); do
+			if [ "$i" -eq 1 ]; then
 				printf "\33[%d;%dH" "$j" "$i";
-				echo -en "${blue}|${reset}";
-			elif (( j == 1 )); then
+				printf "%b" "$blue" "|" "$reset";
+			elif [ "$j" -eq 1 ]; then
 				printf "\33[%d;%dH" "$j" "$i";
-				echo -en "${blue}_${reset}";
-			elif (( j == lines )); then
+				printf "%b" "$blue" "_" "$reset";
+			elif [ "$j" -eq "$lines" ]; then
 				printf "\33[%d;%dH" "$j" "$i";
-				echo -en "${blue}_${reset}";
-			elif (( i == cols)); then
+				printf "%b" "$blue" "_" "$reset";
+			elif [ "$i" -eq "$cols" ]; then
 				printf "\33[%d;%dH" "$j" "$i";
-				echo -en "${blue}|${reset}";
+				printf "%b" "$blue" "|" "$reset";
 			else
 				printf "\33[%d;%dH" "$j" "$i";
-				echo -en " ";
+				printf "%b" " ";
 			fi
 		done
 	done
@@ -42,40 +42,40 @@ clear_screen() {
 info_proc() {
 	# info 1
 	printf "\33[%d;%dH" "$x" "$y";
-	echo -en "${blue}salut les boys${reset}";
+	printf "%b" "$blue" "salut les boys" "$reset";
 	# info 2
-	local x_plus_2=$((x + 3))
+	x_plus_2=$((x + 3))
 	printf "\33[%d;%dH" "$x_plus_2" "$y";
-	echo -en "${reset}${carre_plein}${reset}";
+	printf "%b" "$reset" "$carre_plein" "$reset";
 	# info 3
 }
 
 info_reduite() {
 	#
 	x=2
-	y=3
+	y=2
 	printf "\33[%d;%dH" "$x" "$y";
-	echo -en "${blue}Memory : ${reset}";
-	print_bar_h "${blue}" 10 $((cols - 4)) 50 25
+	printf "%b" "$blue" "Memory : " "$reset";
+	print_bar_h "$blue" 3 $((cols - 3)) 50 25
 }
 
 print_bar_h() {
 	# $1 = couleur
 	# $2 = cols debut de barre
-	# $3 = lines fin de barre
+	# $3 = cols fin de barre
 	# $4 = max var
 	# $5 = current var
-	local res=($1)
+	res="$1"
 	percent=$(( $5 * 100 / $4 ))
-	for((i=$2;i<=$3 - 3;i++)); do
-		if (( ($i * 100) / ($3 - 4) <= percent )); then
-			res+="${carre_plein}";
+	for i in $(seq "$2" "$(( $3 - 3 ))"); do
+		if [ $(( i * 100 / ( $3 - 4 ) )) -le "$percent" ]; then
+			res="$res" "$carre_plein";
 		else
-			res+="${reset}${carre_plein}";
+			res="$res" "$reset" "$carre_plein";
 		fi
 	done
 	printf "\33[%d;%dH" "$2" "$4";
-	echo -en "$res${reset} $percent%";
+	printf "%b" "$res" "$reset" "$percent" "%";
 }
 
 main() {
@@ -89,10 +89,8 @@ main() {
 }
 
 clear_screen
-##info_proc 2 2
-info_reduite
-##print_bar_h "${blue}" 0 10 30 15
+#info_reduite
 
 test_x=$(tput cols)
 test_y=$(tput lines)
-printf "\33[%d;%dH" "$test_x" "$test_y";
+#printf "\33[%d;%dH" "$test_x" "$test_y";
