a5e69d869825f185b437c616442b008656bda720
En3ide <38477527+En3ide@users.noreply.github.com>
2024-10-20 22:35:15 +0200

    rf

--- test/test_grep.sh
+++ test/test_grep.sh
@@ -3,6 +3,7 @@ tmp="3.7G"
 if(($(echo "tmp" | grep -o '[a-zA-Z]*'| head -n 1) == )); then
     entier=$(echo "$tmp" | grep -o '[0-9]*.'| grep -o '[0-9]*' | head -n 1);
     decimal=$(echo "$tmp" | grep -o '[0-9]*.'| grep -o '[0-9]*' | head -n 2 | tail -n 1);
+    
 #echo $tmp
 echo "nb : "
 echo $res
